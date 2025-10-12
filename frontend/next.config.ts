062b003757738d163738bc488a754da245ce8d33
Ved-KidoraLearn <vedkidoralearn@gmail.com>
2025-10-13 03:11:46 +0530

    upadte config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,14 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*", // match any call to /api/*
-        destination: "http://localhost:5000/api/:path*", // forward to Flask
-      },
-    ];
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
