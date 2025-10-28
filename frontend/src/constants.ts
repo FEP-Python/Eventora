93cf6bb85453685a19724596520d3b36b88e07de
Ved-KidoraLearn <vedkidoralearn@gmail.com>
2025-10-28 15:29:25 +0530

    updated env variables

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,2 +1,2 @@
-export const frontend_url = 'http://localhost:3000';
-export const backend_api_url = 'http://localhost:5000/api';
+export const frontend_url = process.env.NEXT_PUBLIC_FRONTEND_URL || 'http://localhost:3000';
+export const backend_api_url = process.env.NEXT_PUBLIC_BACKEND_API_URL || 'http://localhost:5000/api';
